eed53cb44ad9ecacc2b321dea86af32025cb6310
Kolawole Daniel <kolawolecoke@gmail.com>
2023-02-27 14:44:49 +0100

    created user model and admin for for creating and updating users

--- authentication/models.py
+++ authentication/models.py
@@ -1,21 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser,UserManager
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import gettext_lazy as _
 
 
 
 # Create your models here.
-class CustomManager(UserManager):
-    def create(self,email,username,password, **other_fields):
+class CustomManager(BaseUserManager):
+    def create_user(self,email,password, **other_fields):
         
         if not email:
-            raise ValueError(_('You must provide an email address'))
-        if not username:
-            raise ValueError(_('You must provide a username'))
+            raise ValueError(_('You must provide an email address.'))
         
-        email = self.normalize_email(email=email)
+        email = self.normalize_email(email)
         
-        user = self.model(email=email,username=username,**other_fields)
+        user = self.model(email=email,**other_fields)
         
         user.set_password(password)
         
@@ -23,7 +22,8 @@ class CustomManager(UserManager):
         
         return user
     
-    def create_superuser(self, username, email, password, **other_fields):
+    
+    def create_superuser(self,email,password, **other_fields):
         other_fields.setdefault('is_staff', True)
         other_fields.setdefault('is_superuser', True)
         other_fields.setdefault('is_active', True)
@@ -34,24 +34,27 @@ class CustomManager(UserManager):
         if other_fields.get('is_staff') is not True:
             raise ValueError(_('Superuser must be assigned is_staff=True'))
 
-        return self.create_user(email, username, password, **other_fields)
+        return self.create_user(email, password, **other_fields)
+    
+    
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractBaseUser,PermissionsMixin):
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=50,unique=True)
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username','first_name','last_name']
+    REQUIRED_FIELDS = ['first_name','last_name']
     
     objects = CustomManager()
     
     def __str__(self) -> str:
-        return self.username
+        return self.email
 
 
 
