37ff5dfbff459ab30c36549a37abdfb79ef1accb
Kolawole Daniel <kolawolecoke@gmail.com>
2023-09-27 16:52:26 +0100

    google client validator and serializer validator

---
+++ social_auth/google.py
@@ -0,0 +1,20 @@
+from google.auth.transport import requests
+from google.oauth2 import id_token
+
+
+class Google(object):
+    """
+    Google class to fetch user information
+    """
+
+    @staticmethod
+    def validate(auth_token):
+        """queries the google OAuth2 api and returns user information"""
+        try:
+            id_info = id_token.verify_oauth2_token(auth_token, requests.Request())
+
+            if id_info.get("iss") == "accounts.google.com":
+                return id_info
+
+        except:
+            return "The token is either invalid or expired"
--- social_auth/serializers.py
+++ social_auth/serializers.py
@@ -1,8 +1,30 @@
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
+from . import google
+from django.utils.translation import gettext_lazy as _
+from decouple import config
 
 
 class GoogleAuthSerializer(serializers.Serializer):
     auth_token = serializers.CharField()
 
     def validate_auth_token(self, auth_token):
-        pass
+        user_data = google.Google.validate(auth_token)
+        try:
+            user_data["sub"]
+
+        except KeyError:
+            raise serializers.ValidationError(
+                _("The token is either invalid or expired")
+            )
+
+        if user_data.get("aud") != config("GOOGLE_CLIENT_ID"):
+            raise exceptions.AuthenticationFailed("oops, unexpected client")
+
+        user_id = user_data.get("sub")
+        email = user_data.get("email")
+        name = user_data.get("name")
+        provider = user_data.get("provider")
+
+        return register_social_user(
+            user_id=user_id, email=email, name=name, provider=provider
+        )
