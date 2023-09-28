28779278e413d83fa0dfefb4ac97c3d80fe14395
Kolawole Daniel <kolawolecoke@gmail.com>
2023-09-28 21:59:06 +0100

    preparing post_save signals to send welcome mail on user creation

---
+++ authentication/signals.py
@@ -0,0 +1,10 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+@receiver(post_save, sender=User)
+def send_welcome_email(sender, instance, created, **kwargs):
+    pass
