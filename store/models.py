f39780d935aaab43cb215c5c8475bc6d44d8cc43
Kolawole Daniel <kolawolecoke@gmail.com>
2023-11-11 21:47:01 +0100

    product variant

--- store/models.py
+++ store/models.py
@@ -38,42 +38,14 @@ class Category(models.Model):
         super(Category, self).save(*args, **kwargs)
 
 
-class Color(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-
-    class Meta:
-        verbose_name = "Color"
-        verbose_name_plural = "Colors"
-
-    def __str__(self):
-        return self.name
-
-
-class Size(models.Model):
-    name = models.CharField(max_length=10, unique=True)
-
-    class Meta:
-        verbose_name = "Size"
-        verbose_name_plural = "Sizes"
-
-    def __str__(self):
-        return self.name
-
-
 class Product(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    image = models.ImageField(upload_to="images/products/%Y/%m/%d")
-    price = models.DecimalField(max_digits=7, decimal_places=2)
-    colors = models.ManyToManyField(Color, blank=True)
-    sizes = models.ManyToManyField(Size, blank=True)
     description = models.TextField(blank=True, null=True)
     categories = models.ForeignKey(
         "Category", on_delete=models.SET_NULL, related_name="products", null=True
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    in_stock = models.BooleanField(default=False)
-    count_instock = models.PositiveIntegerField(default=0)
     popularity = models.PositiveIntegerField(default=0)
 
     class Meta:
@@ -81,15 +53,23 @@ class Product(models.Model):
         verbose_name_plural = "Products"
         ordering = ("-created_at",)
 
-    def save(self, *args, **kwargs):
-        if self.count_instock == 0:
-            self.in_stock = False
-        else:
-            self.in_stock = True
-        return super().save(*args, **kwargs)
-
     def __str__(self) -> str:
         return self.name
+
+
+class ProductVariant(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    color = models.CharField(max_length=50)
+    size = models.CharField(max_length=10)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    image = models.ImageField(upload_to="images/products/%Y/%m/%d")
+    stock_status = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ["product", "size", "color"]
+
+    def __str__(self) -> str:
+        return f"{self.product.name}-{self.color}-{self.size}"
 
 
 class Review(models.Model):
